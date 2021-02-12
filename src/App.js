fe599592d2df2d5d335771c1cb56a72733904b53
Johnson Tran <johnsontothetran1@gmail.com>
2021-02-12 15:27:53 -0600

    Hello Component
    Created The hello component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Hello from './components/hello'
 
 class App extends React.Component
 {
     render()
     {
         return(<div>
-            <h1>Music App</h1>
+            {Hello}
         </div>)
     }
 }
