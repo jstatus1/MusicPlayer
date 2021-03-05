a674a1cc3e91569b34df227aeb4540c61cfa59a6
Johnson Tran <johnsontothetran1@gmail.com>
2021-03-05 14:48:26 -0600

    Update

---
+++ musicplayer-backend/config/dev.js
@@ -0,0 +1,8 @@
+//Don't Commit This
+
+module.exports = {
+    googleClientID: `289042044535-9gsom7qkv05mlmispgbpk3cnvli4h5gv.apps.googleusercontent.com`,
+    googleClientSecret: 'umIQwb7RgNgTLR4n3n7-Ucgw',
+    cookieKey: 'dsafadsfasdfasdfadsfasdf',
+    postgresqlPassword: 'qweasdzxc123'
+};
