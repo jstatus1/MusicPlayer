d593254b9578a1b2d211b37dd137fbd1f4e61f86
Johnson Tran <johnsontothetran1@gmail.com>
2021-03-19 12:04:44 -0500

    update

--- musicplayer-frontend/src/components/Upload/song-form.js
+++ musicplayer-frontend/src/components/Upload/song-form.js
@@ -9,7 +9,15 @@ export default class SongForm extends React.Component
         genre: null,
         tags: [],
         description: null,
-        caption: null
+        caption: null,
+        genre: {
+            0: ["None", "Custom"],
+            "Music": ["Alternative Rock", "Ambient", "Classical", "Country", "Dance & EDM", "Dancehall", 
+                        "Deep House", "Disco", "Drum & Bass", "Dubstep", "Electronic",  "Folk & Singer-Songwriter", "Hip Hop & Rap", "House", "Indie",
+                    "Jazz & Blues", "Latin", "Metal", "Piano", "Pop", "R&B & Soul", "Reggae", "Reggaeton", "Rock", "Soundtrack", "Techno", "Trance", 
+                    "Trap", "Triphop", "World"],
+            "Audio":["Audiobooks", "Business", "Comedy", "Entertainment", "Learning", "News & Politics", "Religion & Spirituality", "Science", "Sports", "Storytelling", "Technology"]
+        }
     }
 
     imageHandler = (e) => {
@@ -64,6 +72,38 @@ export default class SongForm extends React.Component
         )
     }
 
+    renderGenreList()
+    {
+        var list = [];
+        list.push('<div className="g-scrollable-inner"> ')
+        for (const key in this.state.genre) 
+        {
+
+            list.push('<section className="section_group" id="linkMenu__group"><ul className= "sc-list-nostyle ' + `${key}-list` + '">')
+           
+            if(key != 0)
+            {
+               
+                list.push('<h3 className="sc-border-light-bottom linkMenu__groupTitle sc-type-light" id="linkMenu__groupTitle">' + key + '</h3>')
+            
+            }
+
+            for (var value in this.state.genre[key]) {
+                list.push('<li id="linkMenu__item"> <a class="dropdown-item">' + this.state.genre[key][value] + '</a></li>')
+            }
+                
+            list.push('</ul></section>')
+        }
+
+        list.push('</div>')
+            
+        
+
+        document.getElementsByClassName("dropdown-menu genre-drowdown-menu")
+        
+        return {__html:  list.join('') }
+    }
+
     render()
     {
         return(<React.Fragment>
@@ -79,32 +119,31 @@ export default class SongForm extends React.Component
                                     <input type="email" class="form-control"  onChange={e => this.setState({title:e.target.value})} value={this.state.title} placeholder={"Name Your Track"}/>
                                 </div>
                                 
-                                <div class="dropdown">
-                                    <button class="btn btn-secondary  btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Genre
-                                    </button>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                        <li><a class="dropdown-item">Rap</a></li>
-                                        <li><a class="dropdown-item">Rock</a></li>
-                                        <li><a class="dropdown-item">Taco</a></li>
-                                    </ul>
+                                <div class="dropdown linkMenu__list g-scrollable linkMenu__scrollable g-scrollable-v" id="linkMenu__list">
+                                    
+                                            <button class="btn btn-secondary  btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                               Genre
+                                            </button>
+                                            
+                                                <div className="dropdown-menu genre-drowdown-menu" aria-labelledby="dropdownMenuButton1" dangerouslySetInnerHTML={this.renderGenreList()}>
+                                        
+                                    </div>
                                 </div>
 
                                 <div class="mb-3 mt-3">
-                                    <label for="exampleFormControlInput1" class="form-label">Additional Tags</label>
-                                    <input type="email" class="form-control"  />
+                                    <label for="tag-input" class="form-label">Additional Tags</label>
+                                    <input type="email" class="form-control tag-input"  placeholder="Add tags to describe the genre and mood of your track" />
                                 </div>
 
 
                                 <div class="mb-3 mt-3 ">
-                                    <label for="exampleFormControlInput1" class="form-label">Description</label>
-                                    <textarea class="form-control floatingTextarea2 description-textarea" placeholder="Leave a comment here" ></textarea>
-                                        
+                                    <label for="description-textarea" class="form-label">Description</label>
+                                    <textarea class="form-control floatingTextarea2 description-textarea" placeholder="Leave a comment here" ></textarea>       
                                 </div>
 
                                 <div class="mb-3 mt-3">
-                                    <label for="    exampleFormControlInput1" class="form-label">Caption</label>
-                                    <input type="email" class="form-control"  placeholder="Add a caption to your post(optional)"/>
+                                <label for="exampleFormControlInput1" class="form-label">Caption</label>
+                                    <textarea class="form-control floatingTextarea2 description-textarea" placeholder="Add a caption to you post (optional)" ></textarea>  
                                 </div>
                             </div>
                         </div>
