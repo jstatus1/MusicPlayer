85babee775fcebbfd0a3fd101585b36c09151471
Johnson Tran <johnsontothetran1@gmail.com>
2021-04-05 13:41:34 -0500

    AWS Upload

---
+++ musicplayer-backend/app.js
@@ -0,0 +1,145 @@
+var express = require('express');
+var path = require('path');
+const cors = require("cors");
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var logger = require('morgan');
+var keys = require('./config/keys')
+const passport = require("passport");
+var http = require('http');
+require('./main/services/passportConfig');
+
+//Routes Files
+const indexRouter = require('./main/routes')
+
+var app = express()
+
+
+if(true)
+{
+   app.use(express.static('build'))
+   app.get('/*', (req, res) => {
+     res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+   })
+}else{
+  app.use(
+    cors({
+      origin: "http://localhost:3000", // <-- location of the react app were connecting to
+      credentials: true,
+    }))
+}
+
+//socket 
+
+/*---------------------Middle Ware--------------------------------*/
+app.use(
+    cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+/*---------------------Middle Ware--------------------------------*/
+
+
+
+/*--------------------- Routes --------------------------------*/
+app.use('/', indexRouter)
+//require('./s3/s3')(app);
+require('./main/routes/musicUploadRoutes')(app);
+require('./main/routes/authRoutes')(app);
+
+/*--------------------- Routes --------------------------------*/
+
+
+
+ 
+ /**
+  * Get port from environment and store in Express.
+  */
+ 
+ var port = normalizePort(process.env.PORT || '5010');
+ app.set('port', port);
+ 
+ /**
+  * Create HTTP server.
+  */
+ 
+ var server = http.createServer(app);
+ require('./main/socket').init(server)
+
+
+ /**
+  * Listen on provided port, on all network interfaces.
+  */
+ 
+ server.listen(port);
+ 
+ /**
+  * Normalize a port into a number, string, or false.
+  */
+ 
+ function normalizePort(val) {
+   var port = parseInt(val, 10);
+ 
+   if (isNaN(port)) {
+     // named pipe
+     return val;
+   }
+ 
+   if (port >= 0) {
+     // port number
+     return port;
+   }
+ 
+   return false;
+ }
+ 
+ /**
+  * Event listener for HTTP server "error" event.
+  */
+ 
+ function onError(error) {
+   if (error.syscall !== 'listen') {
+     throw error;
+   }
+ 
+   var bind = typeof port === 'string'
+     ? 'Pipe ' + port
+     : 'Port ' + port;
+ 
+   // handle specific listen errors with friendly messages
+   switch (error.code) {
+     case 'EACCES':
+       console.error(bind + ' requires elevated privileges');
+       process.exit(1);
+       break;
+     case 'EADDRINUSE':
+       console.error(bind + ' is already in use');
+       process.exit(1);
+       break;
+     default:
+       throw error;
+   }
+ }
+ 
+ /**
+  * Event listener for HTTP server "listening" event.
+  */
+ 
+ function onListening() {
+   var addr = server.address();
+   var bind = typeof addr === 'string'
+     ? 'pipe ' + addr
+     : 'port ' + addr.port;
+   debug('Listening on ' + bind);
+ }
+
+module.exports = app;
