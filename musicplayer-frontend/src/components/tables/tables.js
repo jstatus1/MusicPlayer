e019175e41fe7d63a839ee85ddda28fe6aeaadc1
AndreasKalos <78622427+AndreasKalos@users.noreply.github.com>
2021-04-09 16:47:49 -0500

    table stuff

---
+++ musicplayer-frontend/src/components/tables/tables.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+
+class Table extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            Playlists: [
+                {name: 'playlist_name'}
+            ]
+        }
+    }
+
+    renderTableData() {
+        return this.state.Playlists.map((playlist, index) => {
+            const { name } = playlist
+            return (
+                <tr key={playlist}>
+                    <td>{playlist}</td>
+                </tr>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Playlist Dynamic Table</h1>
+                <table id='playlists'>
+                    <tbody>
+                        {this.renderTableData()}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default Table
+
