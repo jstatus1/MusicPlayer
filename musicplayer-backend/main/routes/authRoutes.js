728e381e1f37a54c0486cc03f75fa647b7ed8509
Edward Vo <edwardvo2000@gmail.com>
2021-04-16 01:13:26 -0500

    (error ongoing) connected ProfileEdit page to backend

--- musicplayer-backend/main/routes/authRoutes.js
+++ musicplayer-backend/main/routes/authRoutes.js
@@ -115,6 +115,47 @@ module.exports = app => {
       });
   })
 
-}
+  function ProfileEditLogic(varname, variable, $number) {
+    if (variable === 'nodata') { return ' ' }
+    else {
+      return ` ` + varname + ` = $` + $number 
+    }
+
+  }
+  app.post('/api/updateProfile', async (req, res, next) => {
+      let old_username = req.body.old_username
+      let new_username = req.body.new_username
+      let old_password = req.body.old_password
+      let new_password = req.body.new_password
+      let first_name = req.body.first_name
+      let last_name = req.body.last_name
+      let email = req.body.email
+      let about_me = req.body.about_me
+
+      const hashedOldPassword =  await bcrypt.hash(old_password, 10);
+      const hashedNewPassword =  await bcrypt.hash(new_password, 10);
+
+      value = [old_username, new_username, hashedOldPassword, hashedNewPassword, first_name, last_name, email, about_me ]
+      query = `UPDATE users 
+      SET ${ProfileEditLogic('username', new_username, 2)}, ${ProfileEditLogic('password', new_password, 4)},
+          ${ProfileEditLogic('first_name',first_name,5)}, ${ProfileEditLogic('last_name',last_name,6)},
+          ${ProfileEditLogic('email',email,7)}, ${ProfileEditLogic('about_me',about_me,8)}
+          WHERE username = $1 AND password = $3`
+
+      console.log(query)
+
+      await pool.query(query,value, (q_err, q_res)=> {
+                              if(error) {
+                                q_res.send({status: false})
+                              }
+                              else {
+                                res.send({status: true}) 
+                                }})
+                              }
+                            )
+
+  }
+
+
 
 
--- musicplayer-frontend/src/components/profile/profileedit.js
+++ musicplayer-frontend/src/components/profile/profileedit.js
@@ -1,42 +1,114 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form, Button, Row, Col, Container, } from 'react-bootstrap'
+import axios from 'axios'
 import "./profile.css"
 
 const ProfileEdit = () => {
+    const [old_username, setOldUsername] = useState('');
+    const [new_username, setNewUsername] = useState('');
+    const [old_password, setOldPassword] = useState('');
+    const [new_password, setNewPassword] = useState('');
+    const [first_name, setFirstName] = useState('');
+    const [last_name, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [about_me, setAboutMe] = useState('');
+    const [account_updated, setUpdated] = useState(false);
+     
+
+    const updateAccount = (event) => {
+
+        event.stopPropagation();
+        event.preventDefault();
+
+        const queryInput = {
+            old_username: old_username,
+            new_username: new_username,
+            old_password: old_password,
+            new_password: new_password,
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            about_me: about_me
+        }
+
+        axios.post('/api/updateProfile', {
+        params:{
+            old_username: old_username,
+            new_username: new_username,
+            old_password: old_password,
+            new_password: new_password,
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            about_me: about_me
+        }})
+        .then((res) => {
+            if (res.status === true) { setUpdated(true); }
+        }).catch((error) => {
+            console.log(error)
+        })
+        console.log(queryInput);
+    }
+
     return (
         <div>
             <Container>
-            <Form>
+            <Form onSubmit = {updateAccount}>
                 <Row>
                 <Col md="1"></Col>
                 <Col md="3">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Username"></Form.Control>
+                <Form.Label>Current Username</Form.Label>
+                <Form.Control type="text"
+                              value={old_username}
+                              onChange={(e) => setOldUsername(e.target.value)}></Form.Control>
                 </Col>
-
                 <Col md="1"></Col>
                 <Col md="3">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password"></Form.Control>
+                <Form.Label>New Username</Form.Label>
+                <Form.Control type="text"
+                              value={new_username}
+                              onChange={(e) => setNewUsername(e.target.value)}></Form.Control>
+                </Col>
+                </Row>
+                <Row>
+                <Col md="1"></Col>
+                <Col md="3">
+                <Form.Label>Current Password</Form.Label>
+                <Form.Control type="password"
+                              value={old_password}
+                              onChange={(e) => setOldPassword(e.target.value)}></Form.Control>
+                </Col>
+                <Col md="1"></Col>
+                <Col md="3">
+                <Form.Label>New Password</Form.Label>
+                <Form.Control type="password"
+                              value={new_password}
+                              onChange={(e) => setNewPassword(e.target.value)}></Form.Control>
                 </Col>
             </Row>
             <Row>
                 <Col md="1"></Col>
                 <Col md="3">
                 <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="First Name"></Form.Control>
+                <Form.Control type="text"
+                              value={first_name}
+                              onChange={(e) => setFirstName(e.target.value)}></Form.Control>
                 </Col>
                 <Col md="1"></Col>
                 <Col md="3">
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="Last Name"></Form.Control>
+                <Form.Control type="text"
+                              value={last_name}
+                              onChange={(e) => setLastName(e.target.value)}></Form.Control>
                 </Col>
             </Row>
             <Row>
                 <Col md="1"></Col>
                 <Col md="7">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="text" placeholder="Email address"></Form.Control>
+                <Form.Control type="text"
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}></Form.Control>
                 </Col>
             </Row>
             
@@ -49,7 +121,8 @@ const ProfileEdit = () => {
             <Row>
                 <Col md="1"></Col>
                 <Col md="7">
-                <textarea class="about-me-textbox"></textarea>
+                <textarea class="about-me-textbox"
+                          onchange={(e) => setAboutMe(e.target.value)}></textarea>
                 </Col>
             </Row>
             <Row>
