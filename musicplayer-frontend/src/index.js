55799224493372e538e11c2a4bf11050736b5f50
Johnson Tran <johnsontothetran1@gmail.com>
2021-04-21 16:54:19 -0500

    Revert "fixed refresh"
    This reverts commit 9baca50b984a82e83f12077a580f7ac8f19adedf.

--- musicplayer-frontend/src/index.js
+++ musicplayer-frontend/src/index.js
@@ -8,33 +8,10 @@ import reduxThunk from 'redux-thunk'
 
 import App from './App';
 
-function saveToLocalStorage(state) {
-  try {
-    const serializedState = JSON.stringify(state)
-    localStorage.setItem('state', serializedState)
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-function loadFromLocalStorage() {
-  try {
-    const serializedState = localStorage.getItem('state');
-    if (serializedState === null) return undefined;
-    return JSON.parse(serializedState)
-  } catch (err) {
-    console.log(err)
-    return undefined;
-  }
-}
-
-const persistedState = loadFromLocalStorage();
-
-const store = createStore(reducers, persistedState, composeWithDevTools(
+const store = createStore(reducers, composeWithDevTools(
   applyMiddleware(reduxThunk)
 ));
 
-store.subscribe(() => saveToLocalStorage(store.getState()))
 
 ReactDOM.render(<Provider store={store}>
                   <App />
